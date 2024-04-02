78276a97ff4c6b22276345446b192771ed182f45
Rob Ireton <rireton@googlemail.com>
2024-04-02 11:15:39 +0100

    Remove stake commented code

--- kviewer-client/src/View.tsx
+++ kviewer-client/src/View.tsx
@@ -2,31 +2,9 @@ import { useState } from 'react';
 import { Button } from 'antd';
 import Canvas from './Canvas';
 
-
-// const lowLimit: number = 15, highLimit: number = 30;
-
 const View = () => {
     const [markerSize, updateMarkerSize] = useState(5);
     const [toggleAscend, updateToggleAscend] = useState(true);
-    // const [limit, setLimit] = useState(highLimit);
-
-    // useEffect(() => {
-    //     const interval = setInterval(() => {
-    //         if(limit === highLimit){
-    //             updateMarkerSize(markerSize + 1);
-    //         } else {
-    //             updateMarkerSize(markerSize - 1);
-    //         }
-    //         if(markerSize === highLimit){
-    //             setLimit(lowLimit);
-    //         } else if(markerSize === lowLimit){
-    //             setLimit(highLimit);
-    //         }
-    //     }, 5000);
- 
-    //     // Important to clear the interval
-    //     return () => clearInterval(interval);
-    // }, [limit, markerSize]);
 
     const ascendText = toggleAscend ? "Ascending" : "Descending";
 
