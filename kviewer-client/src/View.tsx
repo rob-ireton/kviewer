48a521eabb3defa18c83c910bdc2c6c80311fa09
Rob Ireton <rireton@googlemail.com>
2024-04-02 14:00:02 +0100

    Add dropdown to drive Canvas for deployments. Events API not working yet as the timepropname will need to be resolved depending on the event type as not all events are same objects... Also, need to tidy up change events as there is a lag when changing which should not be there

--- kviewer-client/src/View.tsx
+++ kviewer-client/src/View.tsx
@@ -1,16 +1,32 @@
 import { useState, useCallback, useEffect } from 'react';
-import { Button } from 'antd';
+import { Button, Dropdown } from 'antd';
 import Canvas from './Canvas';
 import {ApiResponse} from './services/ApiHandler';
 import ApiHandler from './services/ApiHandler';
 
 const apiHandler = new ApiHandler();
 
+const items = [
+  {
+    label: "pods",
+    key: "1",
+  },
+  {
+    label: "deployments",
+    key: "2",
+  },
+  {
+    label: "events",
+    key: "3",
+  }
+];
+
 const View = () => {
     const [markerSize, updateMarkerSize] = useState(5);
     const [toggleAscend, updateToggleAscend] = useState(true);
     const [contentArray, setContentArray] = useState<any>([]);
     const [timePropName, setTimePropName] = useState<string>("startTime");
+    const [contentType, setContentType] = useState<string>(items[0].label);
 
     const ascendText = toggleAscend ? "Ascending" : "Descending";
 
@@ -38,35 +54,82 @@ const View = () => {
 
     // This is the actual function that will be called to refresh the content via server
     const refreshContent = useCallback(async () => {
-        const response = apiHandler.listPods();
+        let response = null;
+        switch (contentType) {
+            case "pods":
+                response = apiHandler.listPods();
+                break;
+            case "deployments":
+                response = apiHandler.listDeployments();
+                break;
+            case "events":
+                response = apiHandler.listCurrentEvents();
+                break;
+        }
+
         // Resolve the promise and handle any errors
-        response
-            .then((resp: ApiResponse) => {
-                // If the arrays are different then update. This is a simple way to check for changes
-                // and update the state only when necessary.
-                if (JSON.stringify(contentArray) !== JSON.stringify(resp.data)){
-                    setContentArray(resp.data);
-                    setTimePropName("startTime");
-                    console.log(resp.data)
-                }
-            })
-            .catch((error) => console.log(error));
-    }, [setContentArray, contentArray, setTimePropName]);
+        if (response) {
+            response
+                .then((resp: ApiResponse) => {
+                    // If the arrays are different then update. This is a simple way to check for changes
+                    // and update the state only when necessary.
+                    if (JSON.stringify(contentArray) !== JSON.stringify(resp.data)){
+                        setContentArray(resp.data);
+                        console.log(resp.data)
+                    }
+                })
+                .catch((error) => console.log(error));
+        }
+    }, [setContentArray, contentArray, contentType]);
+
+    // This useEffect is for the contentType changes to ripple through
+    useEffect(() => {
+        switch (contentType) {
+            case "pods":
+                setTimePropName("startTime");
+                break
+            case "deployments":
+                setTimePropName("creationTime");
+                break;
+        }
+    }, [contentType]);
 
     // This useEffect is for the refresh of content
     useEffect(() => {
         const interval = setInterval(() => {
             refreshContent();
         }, 5000);
-    
+
         // Important to clear the interval
         return () => clearInterval(interval);
     }, [refreshContent]);
+
+    const handleButtonClick = (e: any) => {
+        console.log('click button', e);
+      };
+
+    const handleMenuClick = (e: any) => {
+        const item = items.find((element) => element.key === e.key);
+        if (item) {
+            setContentType(item.label);
+        }
+      };
+    const menuProps = {
+        items,
+        onClick: handleMenuClick,
+      };
 
     return <>
         <Button type="primary" onClick={() => updateMarkerSize(prevData=> (prevData+1))}>Inflate</Button>
         <Button type="primary" onClick={() => updateMarkerSize(markerSize-1)}>Deflate</Button>
         <Button type="primary" onClick={() => updateToggleAscend(prevData=> (!prevData))}>{ascendText}</Button>
+        <Dropdown.Button menu={menuProps} onClick={handleButtonClick}
+            placement="bottom"
+            arrow={{
+                pointAtCenter: true,
+            }}>
+                {contentType}
+        </Dropdown.Button> 
         <Canvas
             markerSize={markerSize}
             ascend={toggleAscend}
