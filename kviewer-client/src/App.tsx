e96bc6af10dd31ab1376694aab74e2f46fc8f55d
Rob Ireton <rireton@googlemail.com>
2024-04-08 11:15:22 +0100

    remove some dead code

--- kviewer-client/src/App.tsx
+++ kviewer-client/src/App.tsx
@@ -28,31 +28,6 @@
 import React from 'react';
 import View from './View';
 
-// Add reverse function
-const reverse = () => {
-  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  console.log(arr);
-
-  //reverse the array using for loop
-  let reversedArr = [];
-  for (let i = arr.length - 1; i >= 0; i--) {
-    reversedArr.push(arr[i]);
-  }
-  console.log(reversedArr);
-
-  // reverse the array using for assignment
-  // let reversedArrAssign : Array<number> = [...arr];
-  // for (let i = 0; i <= Math.floor(arr.length / 2); i++) {
-  //   let temp = arr[i];
-  //   let temp2 = arr[arr.length - 1 - i];
-  //   reversedArrAssign[i] = temp2;
-  //   reversedArrAssign[arr.length - 1 - i] = temp;
-  // }
-  // console.log('TS array :' + reversedArrAssign);
-};
-
-reverse();
-
 const App: React.FC = () => {
 
   return <div className="App">
